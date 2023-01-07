5fa060709d562fe4a4a8f1dbc0cca4f988ead61e
Bogdan Ungureanu <ubogdan@gmail.com>
2023-01-07 21:16:25 +0200

    chore: host mermaid locally

--- terraform/viewer_response.js
+++ terraform/viewer_response.js
@@ -10,8 +10,8 @@ function handler(event) {
     if (headers['content-type'] && headers['content-type'].value == 'text/html') {
         headers['content-security-policy'] = {
             value: "require-trusted-types-for 'script'; default-src 'none'; img-src 'self'; " +
-                "script-src https:  'sha256-ws7am3OYlM2dhKH1jeB5ItmdGlQTRfQSdT3llz8nd8M=' 'sha256-sI9S14ompKIA+MyPxQ84ucUq3p+JKTvKD3E8qfKQvcc=' 'strict-dynamic' 'unsafe-inline'; " +
-                "script-src-elem 'self' https://www.googletagmanager.com 'sha256-SjP4DKbgzKbSIJ6khH2h4w68+MPNPvsOtujPhgl/Mh4=' 'sha256-4CQqINfLuP3nhL220h322geGof5a8+vo3u5unLM/2rw='; " +
+                "script-src https:  'sha256-ws7am3OYlM2dhKH1jeB5ItmdGlQTRfQSdT3llz8nd8M=' 'sha256-sI9S14ompKIA+MyPxQ84ucUq3p+JKTvKD3E8qfKQvcc=' strict-dynamic unsafe-inline; " +
+                "script-src-elem 'self' https://www.googletagmanager.com https://cdn.jsdelivr.net 'sha256-SjP4DKbgzKbSIJ6khH2h4w68+MPNPvsOtujPhgl/Mh4=' 'sha256-4CQqINfLuP3nhL220h322geGof5a8+vo3u5unLM/2rw=' 'sha256-1tqndEcZzFZ8gMUvM8EAVDuiwGkiFpnMg45e2GZGLX8='; " +
                 "style-src 'self' https://fonts.googleapis.com; " +
                 "font-src 'self' https://fonts.gstatic.com; " +
                 "connect-src https://*.google-analytics.com https://www.googletagmanager.com; " +
